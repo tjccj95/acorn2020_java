632fa30fb0519e2c93a300fd53eed0a1ea3d1663
nochangjun <ncj0291@naver.com>
2020-05-28 16:22:03 +0900

    mc08 added

---
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -0,0 +1,13 @@
+package test.main;
+
+public class MainClass08 {
+	public static void main(String[] ar) {
+		//String type의 메소드 사용해보기(다양한 형태로 리턴해줄수있다.)
+		String str="abcde12345";
+		//문자열의 길이
+		int size = str.length();
+		//5번째 인덱스의 문자1개(char)
+		char ch = str.charAt(5);
+		String result=str.toUpperCase();
+	}
+}

dea030289ea2b7cfe759caf36bb57f30130159fa
nochangjun <ncj0291@naver.com>
2020-06-04 12:20:29 +0900

    st09-2 added

--- Step09_Extends/src/test/mypac/SmartPhone.java
+++ Step09_Extends/src/test/mypac/SmartPhone.java
@@ -4,4 +4,12 @@ public class SmartPhone extends HandPhone {
 	public void doInternet() {
 		System.out.println("인터넷을 해요");
 	}
-}
+	//@Override는 부모의 메소드를 재정의한 메소드라고 표시해주는 어노테이션이다.
+	//상위 메서드를 건들지 않는다. 덮어쓰는개념.
+	@Override
+	public void takePicture() {
+		//super.takePicture();
+		System.out.println("1000만 화소의 사진을 찍어요");
+	}
+}	
+
